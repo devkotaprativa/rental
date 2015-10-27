b8b7c3b3f7bcab855031c38e4d654e3b53a8c792
prativa <devkotaprativa@gmail.com>
2015-10-27 15:50:03 +0545

    menu bar added for authenticated user homepage. form for creating new room and editing room created.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,22 +1,42 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
   end
 
   def new
+    @room = Room.new
+    @user = User.find(params[:user_id])
   end
 
   def edit
+    @room = Room.find(params[:id])
   end
 
   def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    flash.notice = "Successfully Deleted"
+    redirect_to user_rooms_path(current_user.id)
   end
 
   def show
   end
 
   def create
+    @room = Room.new(room_params)
+    @room.user_id = params[:user_id]
+    @room.save
+    redirect_to user_rooms_path(current_user.id)
   end
 
   def update
+    @room = Room.find(params[:id])
+    @room.update(room_params)
+    flash.notice = "Successfully #{@room.title} Updated !!"
+    redirect_to user_rooms_path(current_user.id)
+  end
+
+  def room_params
+    params.require(:room).permit(:title, :description, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'rooms/index'
-
-  get 'rooms/new'
-
-  get 'rooms/edit'
-
-  get 'rooms/destroy'
-
-  get 'rooms/show'
-
-  get 'rooms/create'
-
-  get 'rooms/update'
 
   devise_for :users
+  resources :users do
+    resources :rooms
+  end
   resources :rentals
  
 
    devise_scope :user do
   authenticated :user do
-    root :to => 'rentals#index'
+    root :to => 'rooms#index'
   end
   unauthenticated :user do
     root :to => 'rentals#index', as: :unauthenticated_root
