b39b25aaa1e6c3b1084b148950c3a7c0d2607ced
prativa <devkotaprativa@gmail.com>
2015-10-27 13:45:40 +0545

    new controller rooms generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'rooms/index'
+
+  get 'rooms/new'
+
+  get 'rooms/edit'
+
+  get 'rooms/destroy'
+
+  get 'rooms/show'
+
+  get 'rooms/create'
+
+  get 'rooms/update'
+
   devise_for :users
   resources :rentals
  
