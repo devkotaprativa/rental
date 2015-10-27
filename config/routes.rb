ad9e836453e5c26e12476f7f7d432a3e3ab2fb3a
prativa <devkotaprativa@gmail.com>
2015-10-27 11:06:06 +0545

    root path defined for authenticated and unauthenticated users.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
   resources :rentals
  
 
+   devise_scope :user do
+  authenticated :user do
+    root :to => 'rentals#index'
+  end
+  unauthenticated :user do
+    root :to => 'rentals#index', as: :unauthenticated_root
+  end
+end
+
+ 
+
 end
